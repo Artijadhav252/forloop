05b9e13fa33be3d4d619cf1c6746c34c2142263f
artir <artir@REYANSH2021>
2023-02-25 14:29:56 +0530

    Add project files.

---
+++ forloop/Program.cs
@@ -0,0 +1,107 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        FirstNestedLoop1();
+        SecondNestedLoop2();
+        ThirdNestedLoop3();
+        FourthNestedLoop4();
+        FifthNestedLoop5();
+        SixthNestedLoop();
+
+        static void FirstNestedLoop1()
+        {
+            Console.WriteLine("-------------------------------------------");
+
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= 5; j++)
+                {
+                    Console.Write(j);
+                }
+
+                Console.WriteLine();
+
+            }
+
+        }
+
+        static void SecondNestedLoop2()
+        {
+            Console.WriteLine("-------------------------------------------");
+            for (int i = 5; i >= 1; i--)
+            {
+                for (int j = 5; j >= 1; j--)
+                {
+                    Console.Write(j);
+                }
+
+                Console.WriteLine();
+            }
+
+        }
+
+        static void ThirdNestedLoop3()
+        {
+            Console.WriteLine("-------------------------------------------");
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= 5; j++)
+                {
+                    Console.Write(i);
+                }
+
+                Console.WriteLine();
+            }
+
+        }
+
+        static void FourthNestedLoop4()
+        {
+            Console.WriteLine("-------------------------------------------");
+            for (int i = 5; i >= 1; i--)
+            {
+                for (int j = 5; j >= 1; j--)
+                {
+                    Console.Write(i);
+                }
+
+                Console.WriteLine();
+            }
+
+        }
+
+        static void FifthNestedLoop5()
+        {
+            Console.WriteLine("-------------------------------------------");
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+
+        }
+
+
+        static void SixthNestedLoop()
+        {
+            Console.WriteLine("-------------------------------------------");
+            for (int i = 5; i >= 1; i--)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+
+
+        }
+
+    }
+}
